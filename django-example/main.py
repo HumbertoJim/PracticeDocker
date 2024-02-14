94d1e317701dc0aba4bf9730fb8189d231417bf9
HumbertoJim <humbertodjimz@gmail.com>
2024-02-14 17:12:22 -0500

    Update django-example Remove redundant lines

--- django-example/main.py
+++ django-example/main.py
@@ -3,11 +3,9 @@ import subprocess
 import os
 
 BASE_DIR = pathlib.Path(__file__).parent
-print(BASE_DIR)
 
 os.chdir(BASE_DIR)
 
-subprocess.call(['powershell.exe', '-Command','dir'], shell=True)
 subprocess.call(['powershell.exe', '-Command', '.\\venv\\Scripts\\Activate.ps1'], shell=True)
 
 os.chdir(BASE_DIR / 'django-project')
